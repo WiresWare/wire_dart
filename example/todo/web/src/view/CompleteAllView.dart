4fb367ab2206281ee7418f3c4c64012bd80ad36d
vladimircores <vladimir.cores@gmail.com>
2021-04-20 22:11:44 +0300

    Save and restore value for COMPLETE_ALL data key

--- example/todo/web/src/view/CompleteAllView.dart
+++ example/todo/web/src/view/CompleteAllView.dart
@@ -1,13 +1,25 @@
 import 'dart:html';
 
 import 'package:wire/wire.dart';
+import 'package:wire_example_shared/todo/const/DataKeys.dart';
 import 'package:wire_example_shared/todo/const/ViewSignals.dart';
 
 import 'base/DomElementView.dart';
 
 class CompleteAllView extends DomElement {
   CompleteAllView(CheckboxInputElement dom) : super(dom) {
-    dom.onChange.listen(
-        (e) => Wire.send(ViewSignals.COMPLETE_ALL, payload: dom.checked));
+    Wire.add(this, ViewSignals.COMPLETE_ALL_FORCED, _onWireSignalForced);
+    dom.checked = Wire.data(DataKeys.COMPLETE_ALL).value;
+    dom.onChange.listen(_onChecked);
+  }
+
+  void _onChecked (e) {
+    final isChecked = (dom as CheckboxInputElement).checked;
+    Wire.send(ViewSignals.COMPLETE_ALL, payload: isChecked);
+  }
+
+  void _onWireSignalForced(data, wid) {
+    print('> CompleteAllView -> onWireSignalForced: checked = $data');
+    (dom as CheckboxInputElement).checked = data;
   }
 }
