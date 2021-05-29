d0558511829636b3549a99ec5e2ed0dce88da157
vladimircores <vladimir.cores@gmail.com>
2021-05-29 17:29:53 +0300

    node_modules cleanup

--- example/spa/node_modules/@whitespace/storybook-addon-html/out/shared.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.EVENT_CODE_RECEIVED = void 0;
-var EVENT_CODE_RECEIVED = 'html/htmlReceived';
-exports.EVENT_CODE_RECEIVED = EVENT_CODE_RECEIVED;
