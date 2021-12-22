c2f52dbdc7bc38b1cc1f952aa0caa8c0a738af16
vladimircores <vladimir.cores@gmail.com>
2021-12-22 23:49:55 +0300

    Better logging for CounterController in counter example

--- example/counter/web/src/controller/CounterController.dart
+++ example/counter/web/src/controller/CounterController.dart
@@ -6,12 +6,12 @@ import '../const/CounterSignalsKeys.dart';
 class CounterController {
   CounterController() {
     Wire.add(this, CounterSignalsKeys.INCREASE, (dynamic payload, wireId) async {
-      print('> Processor: INCREASE -> handle: ' + payload.toString());
+      print('> Processor: INCREASE -> handle: wireId = ${wireId.toString()}; payload = $payload');
       Wire.data(CounterDataKeys.COUNT, value: (value) => (value ?? 0) + 1);
     });
 
     Wire.add(this, CounterSignalsKeys.DECREASE, (dynamic payload, wireId) async {
-      print('> Processor: DECREASE -> handle: ' + payload.toString());
+      print('> Processor: DECREASE -> handle: wireId = ${wireId.toString()}; payload = $payload');
       Wire.data(CounterDataKeys.COUNT, value: (int value) => value > 0 ? value - 1 : 0);
     });
 
