49041acce1c0273419394ba4fa0c94d03b6594bc
Vladimir Minkin <vladimir.minkin@gmail.com>
2022-11-06 23:42:10 +0300

    Example todo-commands updated with additional code in ClearCompletedTodosCommand to refresh counter

--- example/todo-commands/lib/src/controller/commands/operations/clear_completed_todos_command.dart
+++ example/todo-commands/lib/src/controller/commands/operations/clear_completed_todos_command.dart
@@ -1,6 +1,7 @@
 import 'package:wire/utils/wire_command_with_wire_data.dart';
 import 'package:wire_example_shared/todo/const/data_keys.dart';
 import 'package:wire_example_shared/todo/data/vo/todo_vo.dart';
+import 'package:wire_example_todo_commands/src/controller/commands/operations/check_all_completed_command.dart';
 
 class ClearCompletedTodosCommand extends WireCommandWithWireData<void> {
   ClearCompletedTodosCommand();
@@ -23,7 +24,8 @@ class ClearCompletedTodosCommand extends WireCommandWithWireData<void> {
       await remove(todoId);
     });
     update(DataKeys.LIST_OF_IDS, data: todoIdsList);
-
+    update(DataKeys.COUNT, data: todoIdsList.length);
+    await CheckAllCompletedCommand().execute();
     print('> ClearCompletedTodosCommand -> executed: length = ${todoIdsList.length}');
   }
 }
