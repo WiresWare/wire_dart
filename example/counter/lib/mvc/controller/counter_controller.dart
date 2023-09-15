5a8a4d588cf5836ca47fa4d2c1fc0e8cb4149f36
Vladimir Minkin <vladimir.minkin@gmail.com>
2023-09-15 10:20:09 +0300

    Some code simplification for presentation

--- example/counter/lib/mvc/controller/counter_controller.dart
+++ example/counter/lib/mvc/controller/counter_controller.dart
@@ -1,19 +1,18 @@
 import 'package:wire/wire.dart';
-
 import 'package:wire_example_counter/const/counter_data_keys.dart';
 import 'package:wire_example_counter/const/counter_signals_keys.dart';
 
 class CounterController {
   CounterController() {
-    Wire.add(this, CounterSignalsKeys.DECREASE, (dynamic payload, wireId) async {
-      print('> Processor: DECREASE: wireId = ${wireId}; payload = $payload');
+    Wire.add(this, CounterSignalsKeys.DECREASE, (_, wid) async {
+      // print('> Processor: DECREASE: wireId = ${wireId}; payload = $payload');
       final count = Wire.data(CounterDataKeys.COUNT).value as int?;
       final nextCount = (count ?? 0) > 0 ? (count! - 1) : 0;
       Wire.data(CounterDataKeys.COUNT, value: nextCount);
     });
 
-    Wire.add(this, CounterSignalsKeys.INCREASE, (dynamic payload, wireId) async {
-      print('> Processor: INCREASE: wireId = ${wireId}; payload = $payload');
+    Wire.add(this, CounterSignalsKeys.INCREASE, (_, wid) async {
+      // print('> Processor: INCREASE: wireId = ${wireId}; payload = $payload');
       Wire.data(CounterDataKeys.COUNT, value: (value) {
         final count = value as int?;
         return (count ?? 0) + 1;
